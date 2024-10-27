f9f640f16fa5dd03bbc1730221d33c8eb03437b0
Manisankar Reddy Ravi <manisankarreddyravi3@gmail.com>
2024-10-27 12:13:44 +0000

    Update from the Ec2

--- practice.py
+++ practice.py
@@ -1,5 +1,6 @@
+
 def count_combinations(pieces,target):
-    result=list()
+    result= []#First change from the Ec2 instance.
     def find_combination(start,target,c_omb):
         if target==0:
            result.append(tuple(c_omb))
